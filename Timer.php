2d389a2979f66b3dc0a0864d53bd60c2b77249ec
System Administrator <root@Pan-Radovan.local>
2009-04-15 23:29:48 +0200

    Initial commitance

---
+++ Timer.php
@@ -0,0 +1,28 @@
+<?php
+	/**
+	 * Timer
+	 * Easy benchmark machine
+	 * @author Radovan Paulech
+	 */
+	class Timer {
+		private $startTime;
+		private $result;
+	
+		public function start() {
+			$this->startTime = $this->getMicrotime();
+		}
+		
+		public function stop() {
+			$this->result = $this->getMicrotime() - $this->startTime;
+		}
+		
+		public function getResult() {
+			return $this->result;
+		}
+		
+		private function getMicrotime(){ 
+			list($usec, $sec) = explode(' ', microtime()); 
+			return ((float)$usec + (float)$sec); 
+		}
+	}
+?>
